85dfa55fe5264cb7fc5158ad30087be4b433fb95
MIoT <pavel.kuptsov.2002yandex.ru>
2023-09-01 14:49:45 +0300

    now-1-9-23-14-49

--- fun-facts/Fun-facts-script.js
+++ fun-facts/Fun-facts-script.js
@@ -1,55 +1,113 @@
-//Список фактов
-const facts = [
-    "А",
-    "Б"
-]
+const card = document.querySelector('.card');
+const cardImage = document.querySelector('.card-image');
+const cardText = document.querySelector('.card-text');
+const cardTitle = document.querySelector('.card-text');
+con
 
-const factCard = document.getElementById("fact-card");
-const factText = document.getElementById("fact-text");
-const cardContainer = document.getElementById("card-container")
 
-//Текущий индекс факта в массиве
-let currentFactIndex = 0;
-//Начальная X координата прикосновения
-let touchStartX = 0;
+const cardData = [
+  {
+    image: './',
+    title: 'Шоколадные дороги',
+    text: 'Если бы все батоны шоколада, произведенные за год, выложить в ряд, то эта "шоколадная дорога" могла бы несколько раз обернуть весь земной шар!'
+  },
+  {
+    image: './',
+    title: 'Шоколадные дороги',
+    text: 'Text for Card 2'
+  },
+  // Добавьте другие карточки по аналогии
+];
 
-//Функция для отображения текущего факта
-function displayFact(index) {
-    factText.textContent = facts[index]; 
+let currentIndex = 0;
+let startX = 0;
+let currentTranslateX = 0;
+let isDragging = false;
+let swipeLeftCount = 0;
+let swipeRightCount = 0;
+
+function showCard(index) {
+  cardImage.src = cardData[index].image;
+  cardTitle.textContent = cardData[index].title;
+  cardText.textContent = cardData[index].text;
 }
 
-//Изначальное отобрадение факта
-displayFact(currentFactIndex);
+function resetCardPosition() {
+  card.style.transition = 'transform 0.3s ease';
+  card.style.transform = `translateX(0) rotate(0deg)`;
+}
 
-//Обработчики свайпа
-factCard.addEventListener("touchstart", function(event){
-    // Запоминаем начальную X координату
-    touchStartX = event.touches[0].clientX;
-});
+function handleCardSwipeStart(event) {
+  if (isDragging) return; // Уже перетаскивается
 
-factCard.addEventListener("touchmove", function(event){
-    const touchMoveX = event.touches[0].clientX;
-    const deltaX = touchMoveX - touchStartX;
-    //Сдвигаем карточку
-    factCard.style.transform = `translateX(${deltaX}px)`;
-});
+  startX = event.clientX || event.touches[0].clientX;
+  currentTranslateX = 0;
+  isDragging = true;
 
-factCard.addEventListener("touchend", function(event) {
-    const touchEndX = event.changedTouches[0].clientX;
-    const deltaX = touchEndX - touchStartX;
+  card.style.transition = 'none';
 
-    if (Math.abs(deltaX) >= factCard.offsetWidth * 0.1) {
-        //Убираем карточку и показываем следующий факт
-        factCard.style.transform = "translateX(-100%)";
+  document.addEventListener('mousemove', handleCardSwipeMove);
+  document.addEventListener('touchmove', handleCardSwipeMove);
 
-        setTimeout(() => {
-            // Переходим к следующему факту
-            currentFactIndex = (currentFactIndex + 1) % facts.length;
-            displayFact(currentFactIndex);
-            factCard.style.transform = "translateX(0)";
-        }, 300);
+  document.addEventListener('mouseup', handleCardSwipeEnd);
+  document.addEventListener('touchend', handleCardSwipeEnd);
+}
+
+function handleCardSwipeMove(event) {
+  if (!isDragging) return;
+
+  const currentX = event.clientX || event.touches[0].clientX;
+  const deltaX = currentX - startX;
+
+  currentTranslateX = deltaX;
+  card.style.transform = `translateX(${currentTranslateX}px) rotate(${(currentTranslateX / 10).toFixed(1)}deg)`;
+}
+
+function handleCardSwipeEnd() {
+  if (!isDragging) return;
+
+  isDragging = false;
+
+  document.removeEventListener('mousemove', handleCardSwipeMove);
+  document.removeEventListener('touchmove', handleCardSwipeMove);
+
+  document.removeEventListener('mouseup', handleCardSwipeEnd);
+  document.removeEventListener('touchend', handleCardSwipeEnd);
+
+  card.style.transition = 'transform 0.3s ease';
+
+  if (Math.abs(currentTranslateX) < 100) {
+    resetCardPosition();
+  } else {
+    const direction = currentTranslateX > 0 ? 1 : -1; // 1 for right, -1 for left
+
+    card.style.transform = `translateX(${direction * 300}px) rotate(${direction * 15}deg)`;
+
+    if (direction === 1) {
+      swipeRightCount++;
+
     } else {
-        // Возвращаем карточку в исходное положение
-        factCard.style.transform = "translateX(0)";
+      swipeLeftCount++;
     }
-});
+
+    console.log(`Swipe Right: ${swipeRightCount}, Swipe Left: ${swipeLeftCount}`);
+
+    setTimeout(() => {
+      currentIndex = (currentIndex + (direction === 1 ? 1 : cardData.length - 1)) % cardData.length;
+
+      showCard(currentIndex);
+      resetCardPosition();
+
+      setTimeout(() => {
+        // Снова подключаем обработчики событий для следующей карточки
+        card.addEventListener('mousedown', handleCardSwipeStart);
+        card.addEventListener('touchstart', handleCardSwipeStart);
+      }, 100);
+    }, 300);
+  }
+}
+
+showCard(currentIndex);
+
+card.addEventListener('mousedown', handleCardSwipeStart);
+card.addEventListener('touchstart', handleCardSwipeStart);
