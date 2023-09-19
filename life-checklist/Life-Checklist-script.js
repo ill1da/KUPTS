7d8500cd219b5b7c12fd9cd50b2062cb66036621
MIoT <pavel.kuptsov.2002yandex.ru>
2023-09-19 11:06:49 +0300

    fix

--- life-checklist/Life-Checklist-script.js
+++ life-checklist/Life-Checklist-script.js
@@ -1,23 +1,41 @@
-//1. Импортируем список
-import words from "./Life-Checklist-source/achievements.json" assert {type: "json"};
+// Функция для загрузки JSON-файла
+async function loadJSONFile(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error loading JSON:', error);
+    return null;
+  }
+}
 
 const progressTitleValue = document.querySelector(".progress-title-value");
 
-//Функция для создания элементов
-const createElements = (word) => {
+// Функция для создания элементов на основе данных из JSON-файла
+async function createElements() {
+  const achievementsData = await loadJSONFile("./Life-Checklist-source/achievements.json");
+
+  if (!achievementsData) {
+    console.error('Failed to load achievements data');
+    return;
+  }
+
   let checkedBoxes = {};
   let checkedCount = 0;
 
-  //Получаем данные из локального хранилища
-  if(localStorage.getItem("checkedBoxes")){
+  // Получаем данные из локального хранилища
+  if (localStorage.getItem("checkedBoxes")) {
     checkedBoxes = JSON.parse(localStorage.getItem("checkedBoxes"));
   }
-  if(localStorage.getItem("checkedCount")){
+  if (localStorage.getItem("checkedCount")) {
     checkedCount = localStorage.getItem("checkedCount");
     updateProgressBar(checkedCount);
   }
 
-  words.forEach(word => {
+  achievementsData.forEach(word => {
     let id = `cb-${word}`;
 
     let label = document.createElement("label");
@@ -28,8 +46,8 @@ const createElements = (word) => {
     input.id = id;
     input.classList.add("checkbox");
 
-    //Проверяем, был ли чекбокс активным
-    if(checkedBoxes[id]){
+    // Проверяем, был ли чекбокс активным
+    if (checkedBoxes[id]) {
       label.classList.add("active");
       if (checkedBoxes[input.id]) {
         input.checked = true;
@@ -37,23 +55,23 @@ const createElements = (word) => {
     }
 
     input.addEventListener("change", () => {
-      if(input.checked){
-        navigator.vibrate(20);
+      if (input.checked) {
+        vibrateOnMove();
         label.classList.add("active");
         checkedBoxes[id] = true;
         checkedCount++;
       } else {
-        navigator.vibrate(20);
+        vibrateOnMove();
         label.classList.remove("active");
         delete checkedBoxes[id];
         checkedCount--;
       }
       
-      //Обновляем локальное хранилище
+      // Обновляем локальное хранилище
       localStorage.setItem("checkedBoxes", JSON.stringify(checkedBoxes));
       localStorage.setItem("checkedCount", JSON.stringify(checkedCount));
 
-      //Обновляем прогресс бар
+      // Обновляем прогресс бар
       updateProgressBar(checkedCount);
     });
     
@@ -62,12 +80,18 @@ const createElements = (word) => {
   });
 }
 
-//Функция обнолвения прогресс бара
-const updateProgressBar = (checkedCount) => {
+// Функция обнолвения прогресс бара
+function updateProgressBar(checkedCount) {
   const ProgressBar = document.querySelector(".progress");
   ProgressBar.value = checkedCount;
   progressTitleValue.textContent = checkedCount;
 }
 
-//Вызываем функцию для создания элементов
-createElements(words);
+function vibrateOnMove() {
+  if ("vibrate" in navigator) {
+      navigator.vibrate(20);
+  }
+}
+
+// Вызываем функцию для создания элементов после загрузки JSON-файла
+createElements();
