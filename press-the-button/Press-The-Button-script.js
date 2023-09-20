012d5394c8c7df22d55b5f5572081348bfd04143
MIoT <pavel.kuptsov.2002yandex.ru>
2023-09-20 22:19:51 +0300

    fix press-the-button

--- press-the-button/Press-The-Button-script.js
+++ press-the-button/Press-The-Button-script.js
@@ -1,58 +1,57 @@
-//Элементы
-//Начальный звук
+// Элементы
 const defaultSound = new Audio("./press-the-button-source/sounds/BRUH.mp3");
-//Кнопки
+// Кнопки
 const redButton = document.querySelector("#red-button");
 const resetButton = document.querySelector("#reset-btn");
-//Пользовательский звук
+// Пользовательский звук
 const inputFile = document.querySelector("#change-sound");
 let changeTitle = document.querySelector(".input-file-list");
 
-//Обработчик кнопки
+// Обработчик кнопки
 redButton.addEventListener("click", () => {
   // Получаем сохраненный звук из локального хранилища
   const savedSound = localStorage.getItem("sound");
 
   try {
     // Если звук найден - создаем объект и проигрываем
-    if(savedSound) {
+    if (savedSound) {
       const audio = new Audio(savedSound);
       audio.play();
-    } // Иначе проигрываем звук по умолчанию
-    else {
-      defaultSound.play(); 
+    } else {
+      // Создаем копию начального звука и проигрываем
+      const cloneSound = defaultSound.cloneNode();
+      cloneSound.play();
     }
-  } catch(e) {
+  } catch (e) {
     console.error("Ошибка проигрывания", e);
   }
 });
 
 // Обработчик кнопки Default
 resetButton.addEventListener("click", () => {
-
   // Удаляем сохраненный звук
   localStorage.removeItem("sound");
-  
-  // Проигрываем звук по умолчанию
-  defaultSound.play();
+  // Удаляем имя файла
+  changeTitle.innerHTML = "";
 
+  // Создаем копию начального звука и проигрываем
+  const cloneSound = defaultSound.cloneNode();
+  cloneSound.play();
 });
 
 // Обработчик выбора файла
 inputFile.addEventListener("change", () => {
-
   const file = inputFile.files[0];
-  
+
   // Сохраняем в локальное хранилище
   localStorage.setItem("sound", URL.createObjectURL(file));
-  localStorage.setItem("soundName", file.name); 
+  localStorage.setItem("soundName", file.name);
 
   console.log(file.name);
   changeTitle.innerHTML = file.name;
 });
 
 window.addEventListener("load", () => {
-
   const soundName = localStorage.getItem("soundName");
 
   if (soundName) {
