25e48cefb773dd3bd0925fd9492a77101f66c8f4
ill1da <you@example.copavel.kuptsov.2002yandex.ru>
2023-11-08 15:46:33 +0300

    fix

--- sand-painting/sand-painting-script.js
+++ sand-painting/sand-painting-script.js
@@ -1,38 +1,76 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const lineWidthSlider = document.getElementById('line-width-slider');
-const lineWidthValue = document.getElementById('line-width-value');
-
+let lineWidthSlider = document.getElementById('line-width-slider');
+let lineWidthValue = document.getElementById('line-width-value');
 let painting = false;
+let erasing = false;
+let bucketMode = false;
 let lastX = 0;
 let lastY = 0;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-lineWidthValue.value = lineWidthSlider.value; // Установите начальное значение input type="number"
-
-context.lineWidth = lineWidthSlider.value;
+context.lineWidth = 15;
 context.lineCap = 'round';
 context.strokeStyle = '#825937';
 
 lineWidthSlider.addEventListener('input', () => {
-    const value = Math.min(lineWidthSlider.value, 50);
-    context.lineWidth = value;
-    lineWidthValue.value = value;
+    context.lineWidth = lineWidthSlider.value;
+    lineWidthValue.textContent = lineWidthSlider.value;
 });
 
-lineWidthValue.addEventListener('input', () => {
-    const value = Math.min(lineWidthValue.value, 50);
-    context.lineWidth = value;
-    lineWidthSlider.value = value;
-    lineWidthValue.value = value;
+// Обработчик события для кнопки "Ластик"
+const eraserButton = document.getElementById('eraser');
+eraserButton.addEventListener('click', () => {
+    erasing = true;
+    bucketMode = false;
+    setButtonActive(eraserButton);
+    setButtonInactive(document.getElementById('brush'));
+    setButtonInactive(document.getElementById('bucket'));
+    context.globalCompositeOperation = 'destination-out'; // Устанавливаем режим ластика
+});
+
+// Обработчик события для кнопки "Кисть"
+const brushButton = document.getElementById('brush');
+brushButton.addEventListener('click', () => {
+    erasing = false;
+    bucketMode = false;
+    setButtonActive(brushButton);
+    setButtonInactive(eraserButton);
+    setButtonInactive(document.getElementById('bucket'));
+    context.globalCompositeOperation = 'source-over'; // Возвращаем режим кисти
+});
+
+// Обработчик события для кнопки "Ведро"
+const bucketButton = document.getElementById('bucket');
+bucketButton.addEventListener('click', () => {
+    // Отображаем модальное окно
+    const confirmModal = document.getElementById('confirm-modal');
+    confirmModal.style.display = 'flex';
+
+    document.getElementById('confirm-yes').addEventListener('click', () => {
+        // Если пользователь нажал "Да", очищаем канву
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        confirmModal.style.display = 'none'; // Закрываем модальное окно
+    });
+
+    document.getElementById('confirm-no').addEventListener('click', () => {
+        // Если пользователь нажал "Нет", закрываем модальное окно
+        confirmModal.style.display = 'none';
+    });
 });
 
 function startPosition(e) {
-    painting = true;
-    [lastX, lastY] = [e.clientX, e.clientY];
-    draw(e);
+    if (bucketMode) {
+        // Если режим "Ведро", то отображаем модальное окно
+        const confirmModal = document.getElementById('confirm-modal');
+        confirmModal.style.display = 'block';
+    } else {
+        painting = true;
+        [lastX, lastY] = [e.clientX, e.clientY];
+        draw(e);
+    }
 }
 
 function endPosition() {
@@ -75,17 +113,27 @@ canvas.addEventListener('touchmove', (e) => {
     draw(e);
 });
 
+// Функция для установки активной кнопки
+function setButtonActive(button) {
+    button.classList.add('active');
+}
+
+// Функция для снятия активности с кнопки
+function setButtonInactive(button) {
+    button.classList.remove('active');
+}
+
 // Настройка эффекта песчаных частиц
 particlesJS('particles-js', {
     "particles": {
         "number": {
-            "value": 50000, // Увеличьте это значение
+            "value": 50000,
             "density": {
                 "enable": false,
             }
         },
         "color": {
-            "value": ["#F0C690", "#E3BA7B", "#CC9966", "#F5C896"] // Дополнительные оттенки песка
+            "value": ["#F0C690", "#E3BA7B", "#CC9966", "#F5C896"]
         },
         "shape": {
             "type": "circle"
