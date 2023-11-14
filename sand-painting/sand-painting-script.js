0ed80b645728eaed0013c9d68fa6886e564d5e0b
ill1da <you@example.copavel.kuptsov.2002yandex.ru>
2023-11-14 23:00:41 +0300

    fix

--- sand-painting/sand-painting-script.js
+++ sand-painting/sand-painting-script.js
@@ -106,7 +106,7 @@ function startPosition(e) {
         confirmModal.style.display = 'block';
     } else {
         painting = true;
-        [lastX, lastY] = [e.clientX, e.clientY];
+        updateTouchCoordinates(e);
         draw(e);
     }
 }
@@ -124,19 +124,31 @@ function draw(e) {
 
     if (e.type === 'mousemove' || e.type === 'mousedown') {
         context.beginPath();
-        context.moveTo(lastX, lastY);
+        context.moveTo(lastX.mouse, lastY.mouse);
         context.lineTo(e.clientX, e.clientY);
         context.stroke();
-        [lastX, lastY] = [e.clientX, e.clientY];
+        updateMouseCoordinates(e);
     } else if (e.type === 'touchmove' || e.type === 'touchstart') {
         e.preventDefault();
-        const touch = e.touches[0];
-        context.beginPath();
-        context.moveTo(lastX, lastY);
-        context.lineTo(touch.clientX, touch.clientY);
-        context.stroke();
-        [lastX, lastY] = [touch.clientX, touch.clientY];
+        const touches = e.touches;
+        for (let i = 0; i < touches.length; i++) {
+            context.beginPath();
+            context.moveTo(lastX[touches[i].identifier], lastY[touches[i].identifier]);
+            context.lineTo(touches[i].clientX, touches[i].clientY);
+            context.stroke();
+            updateTouchCoordinates(touches[i]);
+        }
     }
+}
+
+function updateMouseCoordinates(e) {
+    lastX.mouse = e.clientX;
+    lastY.mouse = e.clientY;
+}
+
+function updateTouchCoordinates(touch) {
+    lastX[touch.identifier] = touch.clientX;
+    lastY[touch.identifier] = touch.clientY;
 }
 
 const cursorCircle = document.getElementById('cursor-circle');
@@ -190,7 +202,9 @@ if (isTouchDevice) {
         e.preventDefault();
         startPosition(e);
         cursorCircle.style.display = 'block'; // Показываем круг при касании
-        updateCursorCirclePosition(e.touches[0].clientX, e.touches[0].clientY); // Обновляем позицию круга под пальцем
+        for (let i = 0; i < e.touches.length; i++) {
+            updateTouchCoordinates(e.touches[i]);
+        }
         makeInterfaceElementsTransparent();
     });
 
@@ -199,13 +213,15 @@ if (isTouchDevice) {
         cursorCircle.style.display = 'none'; // Скрываем круг после окончания касания
         restoreInterfaceElementsOpacity();
     });
-}
 
-canvas.addEventListener('touchmove', (e) => {
-    e.preventDefault();
-    draw(e);
-    updateCursorCirclePosition(e.touches[0].clientX, e.touches[0].clientY); // Обновляем позицию круга при перемещении пальца
-});
+    canvas.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        draw(e);
+        for (let i = 0; i < e.touches.length; i++) {
+            updateTouchCoordinates(e.touches[i]);
+        }
+    });
+}
 
 // Функция для установки активной кнопки
 function setButtonActive(button) {
