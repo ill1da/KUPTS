e0becd46918fb212d6e9a874802450f2ecaf9e0e
ill1da <you@example.copavel.kuptsov.2002yandex.ru>
2023-11-15 09:04:22 +0300

    WOW

--- sand-painting/sand-painting-script.js
+++ sand-painting/sand-painting-script.js
@@ -1,279 +1,258 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-let lineWidthSlider = document.getElementById('line-width-slider');
-let lineWidthValue = document.getElementById('line-width-value');
-let painting = false;
-let erasing = false;
-let bucketMode = false;
-let lastX = 0;
-let lastY = 0;
-let actions = []; // Массив для хранения действий
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    let lineWidthSlider = document.getElementById('line-width-slider');
+    let lineWidthValue = document.getElementById('line-width-value');
+    let painting = false;
+    let erasing = false;
+    let bucketMode = false;
+    let lastX = 0;
+    let lastY = 0;
+    let actions = []; // Массив для хранения действий
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-context.lineWidth = 15;
-context.lineCap = 'round';
-context.strokeStyle = '#825937';
+    context.lineWidth = 15;
+    context.lineCap = 'round';
+    context.strokeStyle = '#825937';
 
-lineWidthSlider.addEventListener('input', () => {
-    context.lineWidth = lineWidthSlider.value;
-    lineWidthValue.textContent = lineWidthSlider.value;
-});
-
-function makeInterfaceElementsTransparent() {
-    const interfaceElements = document.querySelectorAll('.interface-element');
-    interfaceElements.forEach(element => {
-        element.style.transition = 'opacity 0.16s ease-in-out'; // Добавляем плавный переход
-        element.style.opacity = 0.2; // Устанавливаем желаемую прозрачность (от 0 - полностью прозрачный до 1 - непрозрачный)
-        element.style.pointerEvents = 'none';
-    });
-}
-
-function restoreInterfaceElementsOpacity() {
-    const interfaceElements = document.querySelectorAll('.interface-element');
-    interfaceElements.forEach(element => {
-        element.style.transition = 'opacity 0.16s ease-in-out'; // Добавляем плавный переход
-        element.style.opacity = 1; // Восстанавливаем полную непрозрачность
-        element.style.pointerEvents = 'auto';
-    });
-}
-
-// Обработчик события для кнопки "Кисть"
-const brushButton = document.getElementById('brush');
-brushButton.addEventListener('click', () => {
-    erasing = false;
-    bucketMode = false;
-    setButtonActive(brushButton);
-    setButtonInactive(eraserButton);
-    setButtonInactive(document.getElementById('bucket'));
-    context.globalCompositeOperation = 'source-over'; // Возвращаем режим кисти
-    cursorCircle.style.borderStyle = 'solid'; // Тонкий круг становится сплошным
-    cursorCircle.style.borderColor = '#17191D'; // И черным
-});
-
-// Обработчик события для кнопки "Ластик"
-const eraserButton = document.getElementById('eraser');
-eraserButton.addEventListener('click', () => {
-    erasing = true;
-    bucketMode = false;
-    setButtonActive(eraserButton);
-    setButtonInactive(document.getElementById('brush'));
-    setButtonInactive(document.getElementById('bucket'));
-    context.globalCompositeOperation = 'destination-out'; // Устанавливаем режим ластика
-    cursorCircle.style.borderStyle = 'dashed'; // Тонкий круг становится пунктирным
-    cursorCircle.style.borderColor = '#17191D'; // И черным
-});
-
-// Обработчик события для кнопки "Ведро"
-const bucketButton = document.getElementById('bucket');
-bucketButton.addEventListener('click', () => {
-    // Отображаем модальное окно
-    const confirmModal = document.getElementById('confirm-modal');
-    confirmModal.style.display = 'flex';
-
-    document.getElementById('confirm-yes').addEventListener('click', () => {
-        // Если пользователь нажал "Да", очищаем канву и обнуляем массив действий
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        actions = [];
-        confirmModal.style.display = 'none'; // Закрываем модальное окно
+    lineWidthSlider.addEventListener('input', () => {
+        context.lineWidth = lineWidthSlider.value;
+        lineWidthValue.textContent = lineWidthSlider.value;
     });
 
-    document.getElementById('confirm-no').addEventListener('click', () => {
-        // Если пользователь нажал "Нет", закрываем модальное окно
-        confirmModal.style.display = 'none';
-    });
-    restoreInterfaceElementsOpacity(); // Восстанавливаем непрозрачность элементов интерфейса
-});
-
-// Обработчик события для кнопки "Отмена"
-const cancelButton = document.getElementById('cancel');
-cancelButton.addEventListener('click', () => {
-    if (actions.length > 0) {
-        // Если есть действия, отменяем последнее и перерисовываем канву
-        actions.pop();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        actions.forEach(action => context.putImageData(action, 0, 0));
+    function makeInterfaceElementsTransparent() {
+        const interfaceElements = document.querySelectorAll('.interface-element');
+        interfaceElements.forEach(element => {
+            element.style.transition = 'opacity 0.16s ease-in-out'; // Добавляем плавный переход
+            element.style.opacity = 0.2; // Устанавливаем желаемую прозрачность (от 0 - полностью прозрачный до 1 - непрозрачный)
+            element.style.pointerEvents = 'none';
+        });
     }
-    restoreInterfaceElementsOpacity(); // Восстанавливаем непрозрачность элементов интерфейса
-});
 
-function startPosition(e) {
-    makeInterfaceElementsTransparent();
-    if (bucketMode) {
+    function restoreInterfaceElementsOpacity() {
+        const interfaceElements = document.querySelectorAll('.interface-element');
+        interfaceElements.forEach(element => {
+            element.style.transition = 'opacity 0.16s ease-in-out'; // Добавляем плавный переход
+            element.style.opacity = 1; // Восстанавливаем полную непрозрачность
+            element.style.pointerEvents = 'auto';
+        });
+    }
+
+    // Обработчик события для кнопки "Кисть"
+    const brushButton = document.getElementById('brush');
+    brushButton.addEventListener('click', () => {
+        erasing = false;
+        bucketMode = false;
+        setButtonActive(brushButton);
+        setButtonInactive(eraserButton);
+        setButtonInactive(document.getElementById('bucket'));
+        context.globalCompositeOperation = 'source-over'; // Возвращаем режим кисти
+        cursorCircle.style.borderStyle = 'solid'; // Тонкий круг становится сплошным
+        cursorCircle.style.borderColor = '#17191D'; // И черным
+    });
+
+    // Обработчик события для кнопки "Ластик"
+    const eraserButton = document.getElementById('eraser');
+    eraserButton.addEventListener('click', () => {
+        erasing = true;
+        bucketMode = false;
+        setButtonActive(eraserButton);
+        setButtonInactive(document.getElementById('brush'));
+        setButtonInactive(document.getElementById('bucket'));
+        context.globalCompositeOperation = 'destination-out'; // Устанавливаем режим ластика
+        cursorCircle.style.borderStyle = 'dashed'; // Тонкий круг становится пунктирным
+        cursorCircle.style.borderColor = '#17191D'; // И черным
+    });
+
+    // Обработчик события для кнопки "Ведро"
+    const bucketButton = document.getElementById('bucket');
+    bucketButton.addEventListener('click', () => {
+        // Отображаем модальное окно
         const confirmModal = document.getElementById('confirm-modal');
-        confirmModal.style.display = 'block';
-    } else {
-        painting = true;
-        updateCoordinates(e);
-        draw(e);
-    }
-}
+        confirmModal.style.display = 'flex';
 
-function endPosition() {
-    restoreInterfaceElementsOpacity();
-    painting = false;
-    context.beginPath();
-    actions.push(context.getImageData(0, 0, canvas.width, canvas.height));
-}
+        document.getElementById('confirm-yes').addEventListener('click', () => {
+            // Если пользователь нажал "Да", очищаем канву и обнуляем массив действий
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            actions = [];
+            confirmModal.style.display = 'none'; // Закрываем модальное окно
+        });
 
-function draw(e) {
-    if (!painting) return;
+        document.getElementById('confirm-no').addEventListener('click', () => {
+            // Если пользователь нажал "Нет", закрываем модальное окно
+            confirmModal.style.display = 'none';
+        });
+        restoreInterfaceElementsOpacity(); // Восстанавливаем непрозрачность элементов интерфейса
+    });
 
-    if (e.type === 'mousemove' || e.type === 'mousedown') {
-        context.lineTo(e.clientX, e.clientY);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
-        updateCoordinates(e);
-    } else if (e.type === 'touchmove' || e.type === 'touchstart') {
-        e.preventDefault();
-        const touches = e.touches;
-        for (let i = 0; i < touches.length; i++) {
-            const id = touches[i].identifier;
-            context.lineTo(touchX[id], touchY[id]);
-            context.stroke();
-            context.beginPath();
-            context.moveTo(touchX[id], touchY[id]);
-            updateCoordinates(touches[i]);
+    // Обработчик события для кнопки "Отмена"
+    const cancelButton = document.getElementById('cancel');
+    cancelButton.addEventListener('click', () => {
+        if (actions.length > 0) {
+            // Если есть действия, отменяем последнее и перерисовываем канву
+            actions.pop();
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            actions.forEach(action => context.putImageData(action, 0, 0));
         }
-    }
-}
+        restoreInterfaceElementsOpacity(); // Восстанавливаем непрозрачность элементов интерфейса
+    });
 
-function updateCoordinates(e) {
-    const id = e.identifier || 'mouse';
-    touchX[id] = e.clientX;
-    touchY[id] = e.clientY;
-}
-
-function updateMouseCoordinates(e) {
-    lastX.mouse = e.clientX;
-    lastY.mouse = e.clientY;
-}
-
-function updateTouchCoordinates(touch) {
-    lastX[touch.identifier] = touch.clientX;
-    lastY[touch.identifier] = touch.clientY;
-}
-
-const cursorCircle = document.getElementById('cursor-circle');
-
-function updateCursorCircleSize() {
-    const cursorSize = context.lineWidth;
-    cursorCircle.style.width = `${cursorSize}px`;
-    cursorCircle.style.height = `${cursorSize}px`;
-}
-
-function updateCursorCirclePosition(x, y) {
-    cursorCircle.style.left = `${x}px`;
-    cursorCircle.style.top = `${y}px`;
-}
-
-// Добавлен код для отслеживания перемещения курсора на всей странице
-document.addEventListener('mousemove', (e) => {
-    updateCursorCirclePosition(e.clientX, e.clientY);
-});
-
-// Обновление размера и позиции круга при изменении размера кисти
-lineWidthSlider.addEventListener('input', () => {
-    context.lineWidth = lineWidthSlider.value;
-    lineWidthValue.textContent = lineWidthSlider.value;
-    updateCursorCircleSize();
-});
-
-canvas.addEventListener('mousemove', (e) => {
-    updateCursorCirclePosition(e.clientX, e.clientY);
-});
-
-canvas.addEventListener('touchmove', (e) => {
-    const touch = e.touches[0];
-    updateCursorCirclePosition(touch.clientX, touch.clientY);
-});
-
-// При старте устанавливаем начальный размер круга
-updateCursorCircleSize();
-
-canvas.addEventListener('mousedown', startPosition);
-canvas.addEventListener('mouseup', endPosition);
-canvas.addEventListener('mousemove', draw);
-
-// Новый код для управления видимостью тонкого круга на телефонах
-let isTouchDevice = ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
-
-if (isTouchDevice) {
-    cursorCircle.style.display = 'none'; // Скрываем круг по умолчанию на телефонах
-
-    canvas.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        startPosition(e);
-        cursorCircle.style.display = 'block'; // Показываем круг при касании
-        for (let i = 0; i < e.touches.length; i++) {
-            updateTouchCoordinates(e.touches[i]);
-        }
+    function startPosition(e) {
         makeInterfaceElementsTransparent();
+        if (bucketMode) {
+            // Если режим "Ведро", то отображаем модальное окно
+            const confirmModal = document.getElementById('confirm-modal');
+            confirmModal.style.display = 'block';
+        } else {
+            painting = true;
+            [lastX, lastY] = [e.clientX, e.clientY];
+            draw(e);
+        }
+    }
+
+    function endPosition() {
+        restoreInterfaceElementsOpacity();
+        painting = false;
+        context.beginPath();
+        // Сохраняем текущее состояние канвы в массив действий
+        actions.push(context.getImageData(0, 0, canvas.width, canvas.height));
+    }
+
+    function draw(e) {
+        if (!painting) return;
+
+        if (e.type === 'mousemove' || e.type === 'mousedown') {
+            context.beginPath();
+            context.moveTo(lastX, lastY);
+            context.lineTo(e.clientX, e.clientY);
+            context.stroke();
+            [lastX, lastY] = [e.clientX, e.clientY];
+        } else if (e.type === 'touchmove' || e.type === 'touchstart') {
+            e.preventDefault();
+            const touch = e.touches[0];
+            context.beginPath();
+            context.moveTo(lastX, lastY);
+            context.lineTo(touch.clientX, touch.clientY);
+            context.stroke();
+            [lastX, lastY] = [touch.clientX, touch.clientY];
+        }
+    }
+
+    const cursorCircle = document.getElementById('cursor-circle');
+
+    function updateCursorCircleSize() {
+        const cursorSize = context.lineWidth;
+        cursorCircle.style.width = `${cursorSize}px`;
+        cursorCircle.style.height = `${cursorSize}px`;
+    }
+
+    function updateCursorCirclePosition(x, y) {
+        cursorCircle.style.left = `${x}px`;
+        cursorCircle.style.top = `${y}px`;
+    }
+
+    // Добавлен код для отслеживания перемещения курсора на всей странице
+    document.addEventListener('mousemove', (e) => {
+        updateCursorCirclePosition(e.clientX, e.clientY);
     });
 
-    canvas.addEventListener('touchend', () => {
-        endPosition();
-        cursorCircle.style.display = 'none'; // Скрываем круг после окончания касания
-        restoreInterfaceElementsOpacity();
+    // Обновление размера и позиции круга при изменении размера кисти
+    lineWidthSlider.addEventListener('input', () => {
+        context.lineWidth = lineWidthSlider.value;
+        lineWidthValue.textContent = lineWidthSlider.value;
+        updateCursorCircleSize();
     });
+
+    canvas.addEventListener('mousemove', (e) => {
+        updateCursorCirclePosition(e.clientX, e.clientY);
+    });
+
+    canvas.addEventListener('touchmove', (e) => {
+        const touch = e.touches[0];
+        updateCursorCirclePosition(touch.clientX, touch.clientY);
+    });
+
+    // При старте устанавливаем начальный размер круга
+    updateCursorCircleSize();
+
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', endPosition);
+    canvas.addEventListener('mousemove', draw);
+
+    // Новый код для управления видимостью тонкого круга на телефонах
+    let isTouchDevice = ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
+
+    if (isTouchDevice) {
+        cursorCircle.style.display = 'none'; // Скрываем круг по умолчанию на телефонах
+
+        canvas.addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            startPosition(e);
+            cursorCircle.style.display = 'block'; // Показываем круг при касании
+            updateCursorCirclePosition(e.touches[0].clientX, e.touches[0].clientY); // Обновляем позицию круга под пальцем
+            makeInterfaceElementsTransparent();
+        });
+
+        canvas.addEventListener('touchend', () => {
+            endPosition();
+            cursorCircle.style.display = 'none'; // Скрываем круг после окончания касания
+            restoreInterfaceElementsOpacity();
+        });
+    }
 
     canvas.addEventListener('touchmove', (e) => {
         e.preventDefault();
         draw(e);
-        for (let i = 0; i < e.touches.length; i++) {
-            updateTouchCoordinates(e.touches[i]);
+        updateCursorCirclePosition(e.touches[0].clientX, e.touches[0].clientY); // Обновляем позицию круга при перемещении пальца
+    });
+
+    // Функция для установки активной кнопки
+    function setButtonActive(button) {
+        button.classList.add('active');
+    }
+
+    // Функция для снятия активности с кнопки
+    function setButtonInactive(button) {
+        button.classList.remove('active');
+    }
+
+    // Настройка эффекта песчаных частиц
+    particlesJS('particles-js', {
+        "particles": {
+            "number": {
+                "value": 50000,
+                "density": {
+                    "enable": false,
+                }
+            },
+            "color": {
+                "value": ["#F0C690", "#E3BA7B", "#CC9966", "#F5C896"]
+            },
+            "shape": {
+                "type": "circle"
+            },
+            "opacity": {
+                "value": 0.7,
+                "random": true
+            },
+            "size": {
+                "value": 3,
+                "random": true
+            },
+            "line_linked": {
+                "enable": false
+            },
+            "move": {
+                "enable": false,
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": false,
+                    "mode": "repulse"
+                }
+            }
         }
     });
-}
-
-// Функция для установки активной кнопки
-function setButtonActive(button) {
-    button.classList.add('active');
-}
-
-// Функция для снятия активности с кнопки
-function setButtonInactive(button) {
-    button.classList.remove('active');
-}
-
-// Настройка эффекта песчаных частиц
-particlesJS('particles-js', {
-    "particles": {
-        "number": {
-            "value": 50000,
-            "density": {
-                "enable": false,
-            }
-        },
-        "color": {
-            "value": ["#F0C690", "#E3BA7B", "#CC9966", "#F5C896"]
-        },
-        "shape": {
-            "type": "circle"
-        },
-        "opacity": {
-            "value": 0.7,
-            "random": true
-        },
-        "size": {
-            "value": 3,
-            "random": true
-        },
-        "line_linked": {
-            "enable": false
-        },
-        "move": {
-            "enable": false,
-        }
-    },
-    "interactivity": {
-        "events": {
-            "onhover": {
-                "enable": false,
-                "mode": "repulse"
-            }
-        }
-    }
-});
