80aef5d678ca656c91db0db07e10af9de7242b7a
ill1da <you@example.copavel.kuptsov.2002yandex.ru>
2023-12-18 20:58:47 +0300

    NW3

--- main/MAIN-script.js
+++ main/MAIN-script.js
@@ -53,3 +53,42 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scrolled');
   }
 });
+
+// Получаем все карточки на странице
+const cards = document.querySelectorAll('.card');
+
+// Добавляем обработчики на каждую карточку
+cards.forEach(card => {
+
+  let defaultTransform = ''; 
+
+  card.addEventListener('mouseover', () => {
+    defaultTransform = card.style.transform; 
+    defaultTransform = card.style.transform = 'scale(1.1)';
+  });
+
+  card.addEventListener('mousemove', e => {
+    
+    let rect = card.getBoundingClientRect();
+    let xCenter = rect.left + rect.width / 2;
+    let yCenter = rect.top + rect.height / 2;
+    
+    let dx = e.clientX - xCenter;
+    let dy = e.clientY - yCenter;
+    
+    let tiltX = dy / rect.height * -15;
+    let tiltY = dx / rect.width * 30;
+    
+    card.style.transform = `${defaultTransform} perspective(1000px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
+  });
+  
+  card.addEventListener('mouseout', () => {
+    card.style.transform = defaultTransform; 
+    defaultTransform = card.style.transform = 'scale(1)'
+  });
+
+});
+
+
+
+
